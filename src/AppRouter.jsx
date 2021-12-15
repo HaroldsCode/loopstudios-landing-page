40454c697605a3448751fd6170537b291c2efc5f
Harold Moreno <haroldmoreno720@gmail.com>
2021-12-15 14:54:40 -0500

    terminado

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,20 +1,16 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import { Home } from './Pages/Home';
-import { Header } from "./Components/Header";
-import { Footer } from "./Components/Footer";
-import { ResponsiveMenu } from './Components/ResponsiveMenu';
 
 export const AppRouter = () => {
 
-  const [responsive, setResponsive] = useState(false);
-
-  return ( responsive 
-    ? <ResponsiveMenu toggle={(e) => setResponsive(toggle => toggle = false)}/>
-    : <>
-        <Header showMenu={(e) => setResponsive(toggle => toggle = true)} />
-        <Home/>
-        <Footer />
-      </>
-  );
+ return (
+   <Router>
+     <Routes>
+       <Route path="/" element={<Home/>}/>
+     </Routes>
+    </Router>
+ )
+  
 };
